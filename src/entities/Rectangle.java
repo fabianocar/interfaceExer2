f51724d84a950047d88e23cb1a00a7ed3e9ee006
Fabianocar <sl.biano28@gmail.com>
2022-04-28 17:58:48 -0300

    Create Project Interface two for exercise

---
+++ src/entities/Rectangle.java
@@ -0,0 +1,38 @@
+package entities;
+
+import enums.Color;
+
+public class Rectangle extends Shape {
+	
+	private Double with;
+	private Double height;
+
+	public Rectangle(Color color, Double with, Double height) {
+		super(color);
+		this.height = height;
+		this.with = with;
+	}
+
+	public Double getWith() {
+		return with;
+	}
+
+	public void setWith(Double with) {
+		this.with = with;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	@Override
+	public double area() {
+		return with * height;
+	}
+
+	
+}
